4866b854f964168e09c2c07a23d43a772620fc18
Silicat04 <stampabin420@proton.me>
2025-06-15 19:37:07 +0000

    Update Simulatore_Algoritmi_Paging.c

--- Simulatore_Algoritmi_Paging.c
+++ Simulatore_Algoritmi_Paging.c
@@ -26,7 +26,18 @@ void right_shift(int dim, int *array);
 //POST: Trova la posizione dell'elemento val all'interno dell'array
 int trova_posizione(int* A, int dim, int val);
 
-int main(int dimRAM, int alg, char PercorsoFile[100], int nFileTraccia){
+int main(int argc, char *argv[]){
+
+    if(argc != 5){
+        printf("Numero incoretto di argomenti!");
+        return 1;
+    }
+
+    int dimRAM = atoi(argv[1]);
+    int alg = atoi(argv[2]);
+    char PercorsoFile[100] = "";
+    strcat(PercorsoFile, argv[3]);
+    int nFileTraccia = atoi(argv[4]);
 
     //Creazione RAM fisica
 
@@ -40,16 +51,21 @@ int main(int dimRAM, int alg, char PercorsoFile[100], int nFileTraccia){
 
     //Lettura file
 
-    FILE *pFile[nFileTraccia]; // Array che contiene i puntatori a tutti i file di tracci
+    FILE *pFile[nFileTraccia]; // Array che contiene i puntatori a tutti i file di traccia
 
     for(int i = 1; i <= nFileTraccia; i++){
-        char str[1];
+        char str[2];
         sprintf(str, "%d", i);
 
         char NomeFile[20] = "";
         strcat(strcat(strcat(NomeFile, "Processo"), str), ".txt");
 
-        pFile[i-1] = fopen(strcat(strcat(PercorsoFile, "/"), NomeFile), "r");
+        char FileCompleto[200] = "";
+        strcat(strcat(strcat(FileCompleto, PercorsoFile), "/"), NomeFile);
+
+        pFile[i-1] = fopen(FileCompleto, "r");
+
+        //printf("%s", FileCompleto);
     }
 
     for(int i = 0; i < nFileTraccia; i++){
@@ -89,15 +105,15 @@ int main(int dimRAM, int alg, char PercorsoFile[100], int nFileTraccia){
                                     FIFO(dimRAM, RAM, ind);
                                 }
                                 else{
-                                    //David
+                                    //LRU
                                     LRU(dimRAM, RAM, ind, 0);
                                 }
                             }
                         }
-                        else{ //Metto la pagina usata in cima alla RAM se sto usando LRU
+                        else{
                             page_hit++;
                             if(alg != 0)
-                            {
+                            { //Metto la pagina usata in cima alla RAM se sto usando LRU
                                 LRU(dimRAM, RAM, ind, 1);
                             }
                         }
@@ -118,15 +134,17 @@ int main(int dimRAM, int alg, char PercorsoFile[100], int nFileTraccia){
     else{
         pf = fopen("LRU.txt","w");
     }
+
     if(pf == NULL){
         printf("Il file non può essere aperto\n");
         return 1;
     }
 
-    fprintf(pf, "FIFO dell'algoritmo:");
-    fprintf(pf, "Page fault totali: %d", page_fault);
-    fprintf(pf, "Page hit totali: %d", page_hit);
+    fprintf(pf, "Page fault totali: %d\n", page_fault);
+    fprintf(pf, "Page hit totali: %d\n", page_hit);
     fprintf(pf, "Fault rate complessivo: %f", (float)page_fault/(page_fault+page_hit));
+
+    fclose(pf);
 
     return 0;
 }
@@ -167,7 +185,7 @@ int Pag_virtuale(int ind, int dimPag){
 
 int ControlloFile(FILE **arr, int n){
     for(int i = 0; i < n; i++){
-        if(*(arr + i) == NULL){
+        if(*(arr + i) != NULL){
             return 1;
         }
     }
